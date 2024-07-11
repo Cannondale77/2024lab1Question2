22aa3842461f3e742e80359e02e8476a653d1e33
Cannondale77 <osullivansg@gmail.com>
2024-07-11 00:01:54 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "mbed.h"
 
-// main() runs in its own thread in the OS
+
+
+int t_1 = 0;
+int t_2 = 1;
+int next_term = 0;
+
+
+int max_terms = 10;
+
 int main()
 {
+    printf("finbonachi series: %d, %d", t_1, t_2);
+    for(int i=0; i<max_terms; i++){
+        next_term =t_1 + t_2;
+        printf("%d, ", next_term);
+    
+
+    }
+    printf("\r\n");
+    
+
     while (true) {
 
     }
