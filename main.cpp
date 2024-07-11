e61ef23cc3e87827e30d2a6fcb424fe016ae77b2
Cannondale77 <osullivansg@gmail.com>
2024-07-11 00:25:40 +0000

    commit 3

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int max_terms = 10;
 
 int main()
 {
-    printf("finbonachi series: %d, %d", t_1, t_2);
+    printf("finbonachi series: %d, %d,\r\n", t_1, t_2);
     for(int i=0; i<max_terms; i++){
         next_term =t_1 + t_2;
         printf("%d, ", next_term);
