a924941812c8c3994dcc76b77e225e6f22096d20
Cannondale77 <osullivansg@gmail.com>
2024-07-23 22:40:46 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -13,17 +13,43 @@ int next_term = 0;
 
 int max_terms = 10;
 
+int fib(int n){
+    //inp: position of the fib sequence
+    //out: the number in that sequene
+
+if    (n == 0)
+
+    return 0;
+
+if (n == 1)
+
+    return 1;
+
+else
+      return(fib(n-1) + fib(n-2));
+}
+
 int main()
 {
-    printf("Finbonachi series: %d, %d,\r\n", t_1, t_2);
-    for(int i=0; i<max_terms; i++){
-        next_term = t_1 + t_2;
-        printf("%d, ", next_term);
+   // printf("Finbonachi series loop: %d, %d,", t_1, t_2);
+   // for(int i=2; i<max_terms; i++){
+   //     next_term = t_1 + t_2;
+   //     printf("%d, ", next_term);
+   //     t_1 = t_2;
+   //     t_2 = next_term;
     
 
-    }
-    printf("\r\n");
+    //}
+   // printf("\r\n");
+
+printf ("finbonachi series recursive:");
+
+for    (int i=0; i<max_terms; i++){
     
+       printf("%d, ", fib(i));
+    }    
+
+printf ("\r\n");
 
     while (true) {
 
